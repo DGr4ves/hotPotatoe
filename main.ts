08deb34959a39b3542ca11211f7ecd00e2a97ece
DGr4ves <102975825+DGr4ves@users.noreply.github.com>
2022-04-15 21:58:48 -0600

    Adds Neopixel compatability

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 function explode () {
     basic.clearScreen()
+    lightFlash()
     led.setBrightness(255)
     led.plot(2, 2)
-    basic.pause(100)
+    lightFlash()
     basic.clearScreen()
     led.plot(2, 1)
     led.plot(1, 2)
     led.plot(3, 2)
     led.plot(2, 3)
-    basic.pause(100)
+    lightFlash()
     basic.clearScreen()
     led.plot(2, 0)
     led.plot(1, 1)
@@ -18,7 +19,7 @@ function explode () {
     led.plot(1, 3)
     led.plot(3, 3)
     led.plot(2, 4)
-    basic.pause(100)
+    lightFlash()
     basic.clearScreen()
     led.plot(0, 0)
     led.plot(1, 0)
@@ -32,18 +33,40 @@ function explode () {
     led.plot(1, 4)
     led.plot(3, 4)
     led.plot(4, 4)
-    basic.pause(100)
+    lightFlash()
     basic.clearScreen()
 }
 input.onButtonPressed(Button.A, function () {
     countDown = 3
-    magicNumber = randint(0, 10)
+    magicNumber = randint(0, 2)
+    strip.clear()
     while (countDown != 0) {
         basic.showNumber(countDown)
-        basic.pause(500)
         countDown += -1
+        range = strip.range(0, 5)
+        range2 = strip.range(5, 5)
+        range3 = strip.range(10, 5)
+        if (countDown == 2) {
+            range.show()
+            range.showColor(neopixel.colors(NeoPixelColors.Red))
+        } else if (countDown == 1) {
+            range2.show()
+            range2.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        } else {
+            range3.show()
+            range3.showColor(neopixel.colors(NeoPixelColors.Green))
+        }
+        basic.pause(500)
     }
     basic.clearScreen()
+    for (let index = 0; index < 2; index++) {
+        strip.show()
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.pause(100)
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        basic.pause(100)
+        strip.clear()
+    }
     basic.showString("Go!")
     while (magicNumber != 0) {
         basic.pause(1000)
@@ -51,8 +74,16 @@ input.onButtonPressed(Button.A, function () {
         displayLights()
     }
     explode()
-    basic.showString("You Loose!")
+    basic.showString("You Lose!")
 })
+function lightFlash () {
+    strip.show()
+    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.pause(100)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.pause(100)
+    strip.clear()
+}
 function displayLights () {
     basic.showLeds(`
         . . . . .
@@ -85,11 +116,40 @@ function displayLights () {
         . . . . .
         . . . . .
         `)
+    strip.clear()
+    strip.show()
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(100)
+    strip.clear()
 }
+let range3: neopixel.Strip = null
+let range2: neopixel.Strip = null
+let range: neopixel.Strip = null
 let countDown = 0
+let strip: neopixel.Strip = null
 let magicNumber = 0
 led.setBrightness(100)
 magicNumber = 0
+strip = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB)
+let Number2 = 0
+while (Number2 < 16) {
+    strip.show()
+    strip.setPixelColor(Number2, neopixel.rgb(randint(0, 65), randint(0, 65), randint(0, 65)))
+    Number2 += 1
+    basic.pause(100)
+}
+while (Number2 > 0) {
+    strip.show()
+    strip.shift(1)
+    Number2 += -1
+    basic.pause(100)
+}
+strip.showRainbow(1, 360)
+while (!(input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
+    strip.show()
+    strip.rotate(1)
+    basic.pause(100)
+}
 basic.forever(function () {
 	
 })
